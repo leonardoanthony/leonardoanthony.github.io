8645820fafa5f197e98130baaad5b0e73f9a0669
Leonardo Anthony <leonardoanthony8@gmail.com>
2020-07-03 16:38:41 -0300

    commit projeto completo

---
+++ pacote-download/leonardoguimaraes/js/functions.js
@@ -0,0 +1,17 @@
+$(function(){
+    var link = $('.link');
+        link.click(function(){
+			var selector = $(this).attr('href');
+			var posicao = $(selector).offset().top;
+            $('html,body').animate({scrollTop:posicao},700); 
+            link.removeClass('selected');
+            $(this).addClass('selected');
+        })
+
+    
+
+
+
+        
+        
+})
